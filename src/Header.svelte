09147e83310a05bf4bcc484c3065a62975361078
san4d <rallensanford@protonmail.com>
2021-04-18 13:39:23 -0700

    Initial splash image

---
+++ src/Header.svelte
@@ -0,0 +1,52 @@
+<div class="social-header">
+	<div id="company-name">Vintage Vineyards</div>
+	<div id="company-icons">
+		<div>
+			<a href="https://www.facebook.com/VintageVineyardLights/">
+				<div>
+					<i class="fab fa-facebook"></i>
+				</div>
+			</a>
+		</div>
+		<div>
+			<a href="https://www.instagram.com/vintage_vineyards/">
+				<div>
+					<i class="fab fa-instagram"></i>
+				</div>
+			</a>
+		</div>
+	</div>
+</div>
+
+<style>
+	.social-header {
+		font-weight: bold;
+		color: antiquewhite;
+		background-color: rgba(0, 0, 0, .7);
+		padding: 1em;
+		display: flex;
+		flex-direction: column;
+	}
+	
+	#company-name {
+		text-align: center;
+		align-content: center;
+	}
+
+	#company-icons {
+		text-align: center;
+		align-content: center;
+		display: flex;
+		flex-direction: row;
+		margin: auto;
+	}
+
+	#company-icons > div {
+		margin: .5em .5em;
+		margin-bottom: 0em;
+	}
+
+	i {
+		color: antiquewhite;
+	}
+</style>
---
+++ src/Splash.svelte
@@ -0,0 +1,25 @@
+<div class="splash">
+    <div class="splash-text">
+        Adding personality to your lighting and your life!
+    </div>
+</div>
+
+<style>
+    .splash {
+        background: transparent url('/barn.png') 0 0 no-repeat fixed;
+        text-align: center;
+        background-size: cover;
+        height: 800px;
+        display: table;
+        width: 100%;
+    }
+    .splash-text {
+        display: table-cell;
+        vertical-align: middle;
+        margin-bottom: 0;
+        font-size: 2em;
+        color: white;
+        font-weight: 500;
+        text-shadow: 0 1px 1px black;
+    }
+</style>
