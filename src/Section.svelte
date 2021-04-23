c17a32ed255fa373fd701ad03f2cf1d2fd34d009
san4d <rallensanford@protonmail.com>
2021-04-22 17:52:53 -0700

    Update section styling

---
+++ src/Section.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let title;
+</script>
+
+<div class="section">
+    <h2>
+        <span>{title}</span>
+    </h2>
+    <slot></slot>
+</div>
+
+<style>
+    h2 {
+        width: 100%; 
+        border-bottom: 2px solid #000; 
+        text-align: center; 
+        margin: 1em 0em; 
+        line-height: 0.1em;
+    } 
+
+    h2 span {
+        background: white;
+        padding:0 1em; 
+    }
+
+    .section {
+        padding: 1em 5em;
+    }
+</style>
